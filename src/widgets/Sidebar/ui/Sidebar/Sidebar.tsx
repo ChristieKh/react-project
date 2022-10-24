b99c0ac23d0b92c6ef360a7231c7d684b92f8104
kristina.kharyushina <kristina.kharyushina@x5.ru>
2022-10-24 21:11:38 +0300

    upgrade sidebar

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,11 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { LangSwitcher } from "widgets/LangSwitcher/ui/LangSwitcher";
 import { ThemeSwitcher } from "widgets/ThemeSwitcher";
 import { useTranslation } from "react-i18next";
+import { Button, ButtonSize, ButtonTheme } from "shared/ui/Button/Button";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
+import AboutIcon from "shared/assets/icons/about.svg";
+import MainIcon from "shared/assets/icons/main.svg";
 import cls from "./Sidebar.module.scss";
 
 interface SidebarProps {
@@ -21,15 +26,41 @@ export const Sidebar = ({ className }: SidebarProps) => {
 		<div
 			data-testid="sidebar"
 			className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-			<button
+			<Button
 				onClick={onToggle}
 				data-testid="sidebar-toggle"
+				className={cls.collapseBtn}
+				theme={ButtonTheme.BACKGROUND_INVERTED}
+				size={ButtonSize.L}
+				square
 			>
-				{t("Переключить")}
-			</button>
+				{collapsed ? ">" : "<"}
+			</Button>
+			<div className={cls.items}>
+				<AppLink
+					theme={AppLinkTheme.SECONDARY}
+					className={cls.item}
+					to={RoutePath.main}>
+					<MainIcon className={cls.icon} />
+					<span className={cls.link}>
+						{t("Главная")}
+					</span>
+				</AppLink>
+				<AppLink
+					theme={AppLinkTheme.SECONDARY}
+					className={cls.item}
+					to={RoutePath.about}
+				>
+					<AboutIcon className={cls.icon} />
+					<span className={cls.link}>
+						{t("О сайте")}
+					</span>
+
+				</AppLink>
+			</div>
 			<div className={cls.switchers}>
 				<ThemeSwitcher/>
-				<LangSwitcher className={cls.lang}/>
+				<LangSwitcher className={cls.lang} short={collapsed} />
 			</div>
 		</div>
 	);
